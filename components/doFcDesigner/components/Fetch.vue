8f3f276fce5fcb943d5bb4090b845264fddc1741
xhyu <xhyu@qeelyn.com>
2022-04-02 16:02:11 +0800

    处理警告提示

--- components/doFcDesigner/components/Fetch.vue
+++ components/doFcDesigner/components/Fetch.vue
@@ -14,7 +14,7 @@ export default defineComponent({
     FormCreate: formCreate.$form(),
   },
   props: {
-    modelValue: { type: Object },
+    modelValue: { type: Object || String },
     to: { type: String },
     formcreateinject: { type: Object },
   },
